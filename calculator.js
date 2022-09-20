dd94247ba98b6ff84956305a1db01ee390400d9c
Árpás Péter <arpaspeter0@gmail.com>
2022-09-20 13:11:07 +0200

    eddig jutottam tovább(semmi)

--- calculator.js
+++ calculator.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 function szamol(){
 
     let sulyszam = document.getElementById("suly").value;
@@ -8,17 +7,3 @@ function szamol(){
     document.getElementById('eredmeny').value = eredmenyszam;
 
 }
-
-=======
-function szamol(){
-
-    let sulyszam = document.getElementById("suly").value;
-    let magassagszam = document.getElementById("magassag").value;
-
-    let eredmenyszam = sulyszam / (magassagszam/100) ** 2;
-    document.getElementById('eredmeny').value = eredmenyszam;
-
-}
-
->>>>>>> beaf6266e5a25b8c23edfb124d8196e41d74196a
-document.getElementById('szamit').addEventListener('click' , szamol);
