beaf6266e5a25b8c23edfb124d8196e41d74196a
Parter-f <56106292+Parter-f@users.noreply.github.com>
2022-09-20 12:56:07 +0200

    Add files via upload

---
+++ calculator.js
@@ -0,0 +1,11 @@
+function szamol(){
+
+    let sulyszam = document.getElementById("suly").value;
+    let magassagszam = document.getElementById("magassag").value;
+
+    let eredmenyszam = sulyszam / (magassagszam/100) ** 2;
+    document.getElementById('eredmeny').value = eredmenyszam;
+
+}
+
+document.getElementById('szamit').addEventListener('click' , szamol);
