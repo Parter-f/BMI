976774c31893de9cf9ece15956a1b39db5c97231
Parter-f <Parter-f@users.noreply.github.com>
2022-09-20 10:59:47 +0000

    deploy: 763a16ec19f47bd464b043e49d74eaef37bef02a

--- calculator.js
+++ calculator.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 function szamol(){
 
     let sulyszam = document.getElementById("suly").value;
@@ -8,4 +9,16 @@ function szamol(){
 
 }
 
+=======
+function szamol(){
+
+    let sulyszam = document.getElementById("suly").value;
+    let magassagszam = document.getElementById("magassag").value;
+
+    let eredmenyszam = sulyszam / (magassagszam/100) ** 2;
+    document.getElementById('eredmeny').value = eredmenyszam;
+
+}
+
+>>>>>>> beaf6266e5a25b8c23edfb124d8196e41d74196a
 document.getElementById('szamit').addEventListener('click' , szamol);
